7d71cb9fcea527fd04a805687fd3c95acbe2d173
GiovanniCastorina <gian.lucact96@gmail.com>
2025-04-29 12:31:18 +0200

    parser problems

--- src/nomad_plugin_qutip/parsers/__init__.py
+++ src/nomad_plugin_qutip/parsers/__init__.py
@@ -1,2 +1,19 @@
 from nomad.config.models.plugins import ParserEntryPoint
 from pydantic import Field
+
+from nomad.config.models.plugins import ParserEntryPoint
+
+
+class NomadParserEntryPoint(ParserEntryPoint):
+    def load(self):
+        from nomad_plugin_qutip.parsers.parser import QutipParser
+
+        return QutipParser(**self.dict())
+
+
+parser = QutipParserEntryPoint(
+    name='QutipParser',
+    description='Parses JSON output files from QuTiP simulations.',
+    mainfile_name_re=r'.+\.qutip\.json$',
+    mainfile_mime_re=r'application/json',
+)
